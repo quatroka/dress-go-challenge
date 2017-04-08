3c4806bb94b53d141c5ef609d5b2f5d91a6e4986
quatroka <monteiro.leonardosantos@gmail.com>
2017-04-08 16:21:06 -0300

    create function to request place data

---
+++ plus-API/places.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+""" File to functions of Google Places API. """
+import googlemaps
+from key import KEY
+
+
+def get_place_data(place):
+    """ Get Google Place data of a place paramter.
+
+        Recive place paramter(string), create a googlemaps
+        client, request your data, and returns your reponse. """
+    gmaps = googlemaps.Client(key=KEY)
+    place_result = gmaps.places(place)
+    return place_result
+
---
+++ plus-API/tests/test_googlemaps.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+""" Test file for Google Places API. """
+from unittest import TestCase, main
+from places import get_place_data
+
+
+class TestGoogleMaps(TestCase):
+    """ Test class for Google Places API. """
+
+
+    def test_get_place_data(self):
+        """ Test for get_place_data_method. """
+        data = get_place_data('Dress & Go')
+        self.assertEqual('Dress & Go', data['results'][0]['name'])
+        self.assertEqual('OK', data['status'])
+        self.assertEqual(-46.6764695, data['results'][0]['geometry']['location']['lng'])
+        self.assertEqual(-23.5949847, data['results'][0]['geometry']['location']['lat'])
+
+
+if __name__ == '__main__':
+    main()
